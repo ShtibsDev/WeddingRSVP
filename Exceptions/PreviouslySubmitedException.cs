fa8d23e1665a1af94159bae115ec07349cb6d3a0
Ofir Stiber <89642985+ShtibsDev@users.noreply.github.com>
2022-05-01 23:06:25 +0300

    Delete Exceptions directory

--- Exceptions/PreviouslySubmitedException.cs
+++
@@ -1,6 +0,0 @@
-namespace WeddingArrival.Exceptions;
-
-public class PreviouslySubmitedException : Exception
-{
-    public PreviouslySubmitedException() : base("Invitee has already submited their response") { }
-}
