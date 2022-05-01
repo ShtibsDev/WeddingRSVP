6ce33354726d1655244a2e8e82b0149e86469d5c
Ofir Stiber <89642985+ShtibsDev@users.noreply.github.com>
2022-05-01 23:07:51 +0300

    Delete Services directory

--- Services/IInviteeService.cs
+++
@@ -1,8 +0,0 @@
-namespace WeddingArrival.Data.Services;
-
-public interface IInviteeService
-{
-    public Task<Invitee> GetInvitee(string phoneNumber);
-    public Task SubmitRsvp(Invitee invitee);
-    public Task<List<Invitee>> GetAllInvitees();
-}
