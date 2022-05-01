63b553d8e60d259e4fa86829c7b7de8d73488309
Ofir Stiber <89642985+ShtibsDev@users.noreply.github.com>
2022-05-01 23:07:14 +0300

    Delete Models directory

--- Models/DatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WeddingArrival.Models;
-
-public class DatabaseSettings
-{
-    public string ConnectionString { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string InviteesCollectionName { get; set; } = null!;
-}
